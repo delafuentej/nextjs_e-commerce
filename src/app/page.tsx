8793b633013208ea64388684a7636c2f2bdcafcd
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-20 11:31:23 +0200

    config. next.js fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { titleFront } from "@/config/fonts";
 
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
      <h1 className="text-5xl">E-commerce: Init project</h1>
+     <h2 className={`${titleFront.className} text-5xl font-bold`}>E-commerce: Init project</h2>
     </main>
   );
 }
