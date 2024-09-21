8688e6237bc11d6b184d6b165440fcf806b8a85c
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-21 13:03:34 +0200

    PageNotFound component

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,19 @@
+import { notFound } from "next/navigation";
 
-export default function Category() {
+
+interface Props {
+  params: {
+    id: string;
+  }
+}
+export default function Category({params}: Props) {
+  const {id} = params;
+  if( id === 'kids'){
+    notFound();
+  }
   return (
     <div>
-      <h1>Category Page</h1>
+      <h1>Category Page: {id}</h1>
     </div>
   );
 }
---
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -0,0 +1,43 @@
+import { titleFront } from '@/config/fonts'
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+export const PageNotFound = () => {
+  return (
+    <div
+      className='flex flex-col-reverse md:flex-row h-[800px] w-full justify-center items-center align-middle'
+    >
+      <div 
+        className='text-center px-5 mx-5'
+      >
+        <h2 className={`${titleFront.className} antialiased text-9xl` }>404</h2>
+        <p className='font-semibold text-3xl'>Whoops! We are very sorry for this!</p>
+        <p
+        className='font-light'
+        >
+          <span className='text-2xl'>Go back to</span>
+          <Link
+          className='font-normal text-2xl p-2 hover:underline hover:bg-purple-500 hover:text-white transition-all ml-2'
+          href='/'
+          >
+            Home
+          </Link>
+        </p>
+      </div>
+
+      {/* image */}
+      <div className='px-5 mx-5 '>
+        <Image 
+          className='p-5 sm:p-0 rounded-full'
+          src='/imgs/logo.jpg'
+          alt='logo'
+          width={550}
+          height={550}
+        />
+
+      </div>
+      
+    </div>
+  )
+}
