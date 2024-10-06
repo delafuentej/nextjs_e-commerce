819eedb606ab1163f8dad77c84d440d7dfb94fa6
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-10-06 13:28:47 +0200

    skeleton inserted while PayPalButton is loading

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,9 +1,23 @@
 'use client';
-import { PayPalButtons } from '@paypal/react-paypal-js';
+
+
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import React from 'react'
 
 export const PayPalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if(isPending){
+    return(
+      <div className='animate-pulse mb-20'>
+        <div className='h-10 bg-purple-200 rounded'></div>
+        <div className='h-10 mt-2 bg-purple-300 rounded'></div>
+      </div>
+    )
+  }
   return (
-   <PayPalButtons />
+    <>
+      <PayPalButtons />
+    </>
   )
 }
