d477674c5bf8e08d8fae435e070107d3354fbbde
Jesús de la Fuente <jfuentelucas@gmail.com>
2025-06-08 14:50:42 +0200

    project update

--- README.md
+++ README.md
@@ -1,15 +1,41 @@
-# Description
+# 🛍️ E-Commerce Fashion Store
 
-# Setting-Up
+Welcome to **E-Commerce Fashion Store** — a modern online clothing store featuring an admin dashboard, user authentication, and PayPal payment integration.
 
-## Bringing-Up in dev mode.
+## 🖼️ Preview
+
+![App Preview](./public/gif/eshop.gif)
+
+---
+
+## ✨ Key Features
+
+- 👤 Customer registration and login
+- Modification of shipping data
+- 🛒 Persistent shopping cart
+- 📦 Order management/Check out
+- 💳 PayPal payment integration
+- 🛠️ **Admin Dashboard**:
+  - Create/Edit products
+  - Manage orders and users
+
+---
+
+## ⚙️ Technologies Used
+
+- **Framework**: Next.js 14
+- **Database & ORM**: Prisma ORM + PostgreSQL _(or your DB of choice)_
+- **Authentication**: NextAuth.js (v5)
+- **Validation**: Zod
+- **Global state**: Zustand
+- **Styling**: Tailwind CSS
+
+## 🚀 How to Run This Project
 
 1. Clone the repository.
-2. Create a copy of the file ```.env.template``` and rename it to ```.env```, and modify the environmet variables.
-3. Install dependencies: ```npm install```
-4. Launch the DB: ```docker compose up -d```
-5. Run prisma's migrations: ```npx prisma migrate dev```
-6. Execute command SEED ```npm run seed```
-7. Launch the application: ```npm run dev```
-
-## Bringing-Up in prod mode. 
+2. Create a copy of the file `.env.template` and rename it to `.env`, and modify the environmet variables.
+3. Install dependencies: `npm install`
+4. Launch the DB: `docker compose up -d`
+5. Run prisma's migrations: `npx prisma migrate dev`
+6. Execute command SEED `npm run seed`
+7. Launch the application: `npm run dev`
--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -1,171 +1,163 @@
-'use server';
+"use server";
 
 import { auth } from "@/auth.config";
 import { Address, Size } from "@/interfaces";
 import prisma from "@/lib/prisma";
 
-
 interface ProductToOrder {
-    productId: string;
-    quantity: number;
-    size: Size;
+  productId: string;
+  quantity: number;
+  size: Size;
 }
 
-export const placeOrder = async( productIds: ProductToOrder[], address: Address) => {
+export const placeOrder = async (
+  productIds: ProductToOrder[],
+  address: Address
+) => {
+  // to obtain userId from server side
+  const session = await auth();
 
-    // to obtain userId from server side
-    const session = await auth();
+  const userId = session?.user.id;
 
-    const userId = session?.user.id;
-
-    // to verify user's session
-    if(!userId){
-        return {
-            ok: false,
-            message: "500 - No user's session"
-        }
-    }
-    
-    console.log({productIds, address, userId})
-   //to obtain info products 
-     const products = await prisma.product.findMany({
-        where: {
-             id: {
-                 in: productIds.map( item => item.productId)
-            }
-        }
-    });
-   // console.log('products',products)
-
-   // calculate the amount
-   const totalItems = productIds.reduce( (count, item) => count + item.quantity,0);
-
-   //console.log({totalItems})
-
-   // subtotal, total, taxes
-   const {subTotal, taxes, total} = productIds.reduce((totals, item) => {
-
-        const productQuantity = item.quantity;
-
-        const product = products.find(product => product.id === item.productId);
- 
-        if(!product) throw new Error(`${item.productId} doesn't exists`)
-
-         //totals
-        const subTotal = product.price * productQuantity; 
-
-         totals.subTotal += subTotal;
-        totals.taxes += subTotal * 0.15;
-         totals.total += subTotal * 1.15;
-
-         return totals;
-        
-    }, {subTotal: 0, taxes: 0, total:0})
-
- //  console.log({subTotal, taxes, total})
-
- // create the  prisma database transaction : Order, OrderAddress, OrderItem
- try{
-    const prismaTx = await prisma.$transaction( async(tx) => {
-
-        // update stock products
-        const updatedProductsPromises = products.map( (product)=> {
-
-            // to accumulate the values 
-            const productQuantity = productIds.filter(
-                item => item.productId === product.id
-            ).reduce((acc, it )=> it.quantity + acc ,0);
-
-            if (productQuantity === 0) {
-                //this error should not happen
-                throw new Error(`${product.id} has no defined quantity`)
-            };
-            return tx.product.update({
-                where: {
-                    id: product.id
-                },
-                data: {
-                   // inStock: product.inStock - productQuantity // incorrect update: not to do in case there are more customers trying to buy the same products at the same time.
-                    inStock : {
-                        decrement: productQuantity,
-                    }
-                }
-            });
-        });
-
-        const updatedProducts = await Promise.all(updatedProductsPromises);
-        // check the updatedProducts array for any order-product that is out of stock.
-        updatedProducts.forEach( item => {
-            if(item.inStock < 0){
-                throw new Error(`${item.title} does not have sufficient stock`)
-            }
-        })
-
-
-        const {country, userId: _userId, id,...restAddress} = address;
-         
-        console.log('restAddress',{restAddress})
-  
-
-
-
-        // create order - Header - Details
-         const order = await tx.order.create({
-            data: {
-                userId: userId,
-                totalItems: totalItems,
-                subTotal: subTotal,
-                taxes: taxes,
-                total: total,
-                
-
-                OrderItem:{
-                    createMany: {
-                        data: productIds.map( (item)=> ({
-                            quantity: item.quantity,
-                            size: item.size,
-                            productId: item.productId,
-                            price: products.find( product => product.id === item.productId)?.price ?? 0,
-                        }))
-                    }
-                }
-            }
-        }); 
-        // validate, if price  = 0, throw error
-
-        // create address order
-        const orderAddress = await tx.orderAddress.create({
-            data: {
-                ...restAddress,
-                  countryId: country,
-                  orderId: order.id,
-                
-             }
-         })
-//         console.log({orderAddress})
-        return {
-            updatedProducts: updatedProducts,
-            order: order,
-            orderAddress: orderAddress,
-           
-           
-        }
-   });
-
-   return {
-    ok: true,
-    order: prismaTx.order,
-    prismaTx: prismaTx,
-   }
-
- }catch (error: any){
+  // to verify user's session
+  if (!userId) {
     return {
-        ok: false,
-        message: error?.message,
-    }
- }
-   
-   
+      ok: false,
+      message: "500 - No user's session",
+    };
+  }
 
-}
+  console.log({ productIds, address, userId });
+  //to obtain info products
+  const products = await prisma.product.findMany({
+    where: {
+      id: {
+        in: productIds.map((item) => item.productId),
+      },
+    },
+  });
+  // console.log('products',products)
 
+  // calculate the amount
+  const totalItems = productIds.reduce(
+    (count, item) => count + item.quantity,
+    0
+  );
+
+  //console.log({totalItems})
+
+  // subtotal, total, taxes
+  const { subTotal, taxes, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+
+      const product = products.find((product) => product.id === item.productId);
+
+      if (!product) throw new Error(`${item.productId} doesn't exists`);
+
+      //totals
+      const subTotal = product.price * productQuantity;
+
+      totals.subTotal += subTotal;
+      totals.taxes += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, taxes: 0, total: 0 }
+  );
+
+  //  console.log({subTotal, taxes, total})
+
+  // create the  prisma database transaction : Order, OrderAddress, OrderItem
+  try {
+    const prismaTx = await prisma.$transaction(async (tx) => {
+      // update stock products
+      const updatedProductsPromises = products.map((product) => {
+        // to accumulate the values
+        const productQuantity = productIds
+          .filter((item) => item.productId === product.id)
+          .reduce((acc, it) => it.quantity + acc, 0);
+
+        if (productQuantity === 0) {
+          //this error should not happen
+          throw new Error(`${product.id} has no defined quantity`);
+        }
+        return tx.product.update({
+          where: {
+            id: product.id,
+          },
+          data: {
+            // inStock: product.inStock - productQuantity // incorrect update: not to do in case there are more customers trying to buy the same products at the same time.
+            inStock: {
+              decrement: productQuantity,
+            },
+          },
+        });
+      });
+
+      const updatedProducts = await Promise.all(updatedProductsPromises);
+      // check the updatedProducts array for any order-product that is out of stock.
+      updatedProducts.forEach((item) => {
+        if (item.inStock < 0) {
+          throw new Error(`${item.title} does not have sufficient stock`);
+        }
+      });
+
+      const { country, ...restAddress } = address;
+
+      console.log("restAddress", { restAddress });
+
+      // create order - Header - Details
+      const order = await tx.order.create({
+        data: {
+          userId: userId,
+          totalItems: totalItems,
+          subTotal: subTotal,
+          taxes: taxes,
+          total: total,
+
+          OrderItem: {
+            createMany: {
+              data: productIds.map((item) => ({
+                quantity: item.quantity,
+                size: item.size,
+                productId: item.productId,
+                price:
+                  products.find((product) => product.id === item.productId)
+                    ?.price ?? 0,
+              })),
+            },
+          },
+        },
+      });
+      // validate, if price  = 0, throw error
+
+      // create address order
+      const orderAddress = await tx.orderAddress.create({
+        data: {
+          ...restAddress,
+          countryId: country,
+          orderId: order.id,
+        },
+      });
+      //         console.log({orderAddress})
+      return {
+        updatedProducts: updatedProducts,
+        order: order,
+        orderAddress: orderAddress,
+      };
+    });
+
+    return {
+      ok: true,
+      order: prismaTx.order,
+      prismaTx: prismaTx,
+    };
+  } catch (error: any) {
+    return {
+      ok: false,
+      message: error?.message,
+    };
+  }
+};
